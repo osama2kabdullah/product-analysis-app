cd65563dc64d2ca58228b40fa5c3a8610db99cfb
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-11 14:25:56 +0600

    review card design done

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import useReview from '../hooks/useReview';
+import ReviwCard from './ReviwCard';
 
 const Review = () => {
+    const [reviews, setReviews] = useReview();
+    console.log(reviews);
     return (
-        <div>
-            <h1>this is review</h1>
+        <div className='m-8 grid grid-cols-2'>
+            {
+                reviews.map(review=><ReviwCard key={review._id} review={review}></ReviwCard>)
+            }
         </div>
     );
 };
